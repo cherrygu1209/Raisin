1d4277e22d67e3ab2ed5bfdd1efbca8de39e32bf
cherrygu <cherrygu1209@gmail.com>
2017-12-01 11:38:45 +0800

    Initial commit

---
+++ frontend/assets/AppAsset.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+
+    public static function addJavaScript($view,$jsfile){
+        $view->registerJsFile($jsfile,[AppAsset::className(),'depends'=>'frontend\assets\AppAsset']);
+    }
+
+    public static function addCss($view,$cssfile){
+        $view->registerCssFile($cssfile,[AppAsset::className(),'depends'=>'frontend\assets\AppAsset']);
+    }
+}
---
+++ frontend/views/site/say.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cherry
+ * Date: 09/10/2017
+ * Time: 2:53 PM
+ */
+
+use yii\helpers\Html;
+
+?>
+
+<?=Html::encode($message) ?>
