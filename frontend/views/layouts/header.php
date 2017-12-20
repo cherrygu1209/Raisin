80aeec80753062a7d960518607975678deea9f6e
cherrygu <cherrygu1209@gmail.com>
2017-12-20 16:30:44 +0800

    Add user drop down menu after user login

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -3,38 +3,11 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\helpers\Url;
 use yii\helpers\Html;
+//$imagePath = '/'.Yii::$app->user->identity->image;
+?>
+<header class="main-header">
 
-/*    NavBar::begin([
-        'brandLabel' => 'My Company',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();*/
-    ?>
+    <!--<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">-->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
         <a class="navbar-brand" href="<?=Url::to(['/site/index'])?>">GoRaisin</a>
@@ -68,35 +41,30 @@ use yii\helpers\Html;
             </li>
             <?php } else {
                 echo'<li class="nav-item"><a class="btn btn-info btn-xs" href="'.Url::to(['/campaign/create']).'">Start a Campaign</a></li>';
-                echo'<li class="nav-item"><a class="nav-link" href="'.Url::to(['#']).'">My Profile</a></li>';
-                echo '<li class="nav-item"><a class="nav-link">'.
-                Html::beginForm(['/site/logout']);
-                echo Html::submitButton('LOGOUT('.Yii::$app->user->identity->username.')', ['class'=>'btn-link logout']);
-                echo Html::endForm().'</a></li>';
-                
-                //'<li class="nav-item"><a class="nav-link" href="'.Url::to(['/site/signup']).'">Logout('.Yii::$app->user->identity->username.')</a></li>';
-            }?>
+            ?>
+              <nav class="menu">
+                  <ul class="clearfix">
+                      <li>
+                          <a href="#"><?= Yii::$app->user->identity->username?></a>
+
+                          <ul class="sub-menu">
+                              <li><a href="#">Profile</a></li>
+                              <li><a href="#">Portfolio</a></li>
+                              <li><a href="#">Campaign</a></li>
+                              <li><a href="#">My Wallet</a></li>
+                              <li><?= Html::a(
+                                      'Logout',
+                                      ['/site/logout'],
+                                      ['data-method' => 'post', 'class' => 'btn btn-default btn-flat']
+                                  ) ?></li>
+                          </ul>
+                      </li>
+                  </ul>
+              </nav>
+            <?php } ?>
           </ul>
         </div>
       </div>
     </nav>
 
-<!--<div id="header">
-      <div id="logo">
-        <div id="logo_text">
-           class="logo_colour", allows you to change the colour of the text 
-          <h1><a href="index.html">Go<span class="logo_colour">Raisin</span></a></h1>
-          <h2>Tagline</h2>
-        </div>
-      </div>
-      <div id="menubar">
-        <ul id="menu">
-           put class="selected" in the li tag for the selected page - to highlight which page you're on 
-          <li><a href=<?=Url::to(['/site/index'])?>>Whitepaper</a></li>
-          <li><a href=<?=Url::to(['/site/index'])?>>Team</a></li>
-          <li><a href=<?=Url::to(['/site/index'])?>>Roadmap</a></li>
-          <li><a href=<?=Url::to(['/site/index'])?>>FAQ</a></li>
-          <li class="selected"><a href=<?=Url::to(['/campaign/create'])?>>Start a Campaign</a></li>
-        </ul>
-      </div>
-    </div>-->
+</header>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Raisin';
 $this->registerCssFile('vendor/bootstrap/css/bootstrap.min.css');
 $this->registerCssFile('vendor/font-awesome/css/font-awesome.min.css');
 $this->registerCssFile('https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic');
@@ -94,63 +94,4 @@ $this->registerJsFile('js/clean-blog.min.js');
             </div>
         </div>
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <!--<div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>-->
 </div>
